42a146a4f42ea61363a40ee1cc34dacf64f49170
Akshay Gupta <akgcodes@gmail.com>
2021-11-30 23:47:51 -0500

    Some issues with package installation -- working locally

--- bayesian_target/__init__.py
+++ bayesian_target/__init__.py
@@ -1,11 +1,10 @@
 """Import path."""
 
 __version__ = "0.1.0"
-__description__ = "Bayesian target encoding with scikit-learn and scipy"
 
 from typing import List
 
-from bayesian_target.encoder import BayesianTargetEncoder
-from bayesian_target.ensemble import BayesianTargetEstimator
+from .encoder import BayesianTargetEncoder
+from .ensemble import BayesianTargetEstimator
 
 __all__: List[str] = ["BayesianTargetEncoder", "BayesianTargetEstimator"]
