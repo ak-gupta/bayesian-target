28d5f068c546e46fdac3b763b506bb1b4a0c4c57
Akshay Gupta <akgcodes@gmail.com>
2021-12-01 21:14:12 -0500

    Additional testing

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.testing import assert_allclose, assert_array_equal
 import pandas as pd
 import pytest
+import scipy.stats
 from sklearn.utils.estimator_checks import check_estimator
 
 from bayte.encoder import (
@@ -15,19 +16,64 @@ def test_encoder_validity():
     """Test the validity against the scikit-learn API."""
     check_estimator(BayesianTargetEncoder(dist="bernoulli"))
 
-@pytest.mark.parametrize(
-    "dist,target,params",
-    [
-        ("bernoulli", np.array([0, 1, 1, 0, 1, 1, 0, 1, 0, 1]), (0.6, 0.4)),
-        ("multinomial", np.array([0, 1, 2, 1, 0, 1, 1, 2, 2, 1]), (2, 5, 3)),
-    ]
-)
-def test_init_prior(dist, target, params):
-    """Test initializing the prior distribution."""
-    out = _init_prior(dist, target)
 
-    assert out == params
+def test_init_prior_bernoulli():
+    """Test initializing the prior distribution with a bernoulli likelihood."""
+    y = np.array([0, 1, 1, 0, 1, 1, 0, 1, 0, 1])
+    out = _init_prior("bernoulli", y)
 
+    assert out == (0.6, 0.4)
+
+
+def test_init_prior_multinomial():
+    """Test initializing the prior distribution with a multinomial likelihood."""
+    y = np.array([0, 1, 2, 1, 0, 1, 1, 2, 2, 1])
+    out = _init_prior("multinomial", y)
+
+    assert out == (2, 5, 3)
+
+
+def test_init_prior_exponential():
+    """Test initializing the prior distribution with an exponential likelihood."""
+    y = scipy.stats.expon(2).rvs(size=100)
+    out = _init_prior("exponential", y)
+
+    assert out == (101, np.sum(y))
+
+
+def test_init_prior_gamma():
+    """Test initializing the prior distribution with a gamma likelihood."""
+    np.random.seed(42)
+    y = scipy.stats.gamma(3).rvs(size=1000)
+    out = _init_prior("gamma", y)
+
+    assert np.abs(out[0]/1000 - 3) <= 1
+    assert out[1] == 0
+    assert out[2] == np.sum(y)
+
+
+def test_init_prior_invgamma():
+    """Test initializing the prior distribution with an inverse gamma likelihood."""
+    np.random.seed(42)
+    y = scipy.stats.invgamma(5).rvs(size=1000)
+    out = _init_prior("invgamma", y)
+
+    assert np.abs(out[0]/1000 - 5) <= 1
+    assert out[1] == 0
+    assert out[2] == np.sum(y)
+
+def test_fit_invalid_dist():
+    """Test raising an error with an invalid likelihood."""
+    df = pd.DataFrame(
+        {
+            "x1": [0, 1, 2, 1, 0, 1, 2, 3, 3, 2],
+            "y": [0, 1, 1, 0, 1, 0, 0, 1, 1, 0]
+        }
+    )
+
+    encoder = BayesianTargetEncoder(dist="fake")
+    with pytest.raises(NotImplementedError):
+        encoder.fit(df[["x1"]], df["y"])
 
 def test_bernoulli_fit():
     """Test fitting the encoder with a binary classification task."""
@@ -57,6 +103,12 @@ def test_bernoulli_fit():
         )
     )
 
+    # Test parallel
+    encoder.set_params(n_jobs=2)
+    encoder.fit(df[["x1"]], df["y"])
+
+    assert encoder.prior_params_ == (0.5, 0.5)
+
 
 def test_multinomial_fit():
     """Test fitting the encoder with a multinomial classification task."""
@@ -83,6 +135,12 @@ def test_multinomial_fit():
             ]
         )
     )
+
+    # Test parallel
+    encoder.set_params(n_jobs=2)
+    encoder.fit(df[["x1"]], df["y"])
+
+    assert encoder.prior_params_ == (6, 9, 5)
 
 
 def test_multinomial_fit_missing_classes():
@@ -124,15 +182,18 @@ def test_transform_bernoulli():
     encoder.fit(df[["x1"]], df["y"])
 
     out = encoder.transform(df[["x1"]])
-
-    assert_allclose(
-        out.ravel(),
-        np.array(
-            [0.5, 0.375, 0.375, 0.375, 0.5, 0.375, 0.375, 0.833333, 0.833333, 0.375]
-        ),
-        rtol=1e-5
+    expected = np.array(
+        [0.5, 0.375, 0.375, 0.375, 0.5, 0.375, 0.375, 0.833333, 0.833333, 0.375]
     )
 
+    assert_allclose(out.ravel(), expected, rtol=1e-5)
+
+
+    # Test parallel transform
+    encoder.set_params(n_jobs=2)
+    out = encoder.transform(df[["x1"]])
+
+    assert_allclose(out.ravel(), expected, rtol=1e-5)
 
 def test_transform_multinomial():
     """Test transforming with a multinomial likelihood."""
@@ -147,32 +208,35 @@ def test_transform_multinomial():
     encoder.fit(df[["x1"]], df["y"])
 
     out = encoder.transform(df[["x1"]])
-
-    assert_allclose(
-        out,
-        np.array(
-            [
-                [0.36, 0.44, 0.2],
-                [0.26923, 0.423077, 0.307692],
-                [0.28, 0.52, 0.2],
-                [0.28, 0.52, 0.2],
-                [0.28, 0.52, 0.2],
-                [0.26923, 0.423077, 0.307692],
-                [0.26923, 0.423077, 0.307692],
-                [0.291667, 0.416667, 0.291667],
-                [0.291667, 0.416667, 0.291667],
-                [0.26923, 0.423077, 0.307692],
-                [0.28, 0.52, 0.2],
-                [0.291667, 0.416667, 0.291667],
-                [0.291667, 0.416667, 0.291667],
-                [0.28, 0.52, 0.2],
-                [0.36, 0.44, 0.2],
-                [0.36, 0.44, 0.2],
-                [0.36, 0.44, 0.2],
-                [0.26923, 0.423077, 0.307692],
-                [0.36, 0.44, 0.2],
-                [0.26923, 0.423077, 0.307692],
-            ]
-        ),
-        rtol=1e-5
+    expected = np.array(
+        [
+            [0.36, 0.44, 0.2],
+            [0.26923, 0.423077, 0.307692],
+            [0.28, 0.52, 0.2],
+            [0.28, 0.52, 0.2],
+            [0.28, 0.52, 0.2],
+            [0.26923, 0.423077, 0.307692],
+            [0.26923, 0.423077, 0.307692],
+            [0.291667, 0.416667, 0.291667],
+            [0.291667, 0.416667, 0.291667],
+            [0.26923, 0.423077, 0.307692],
+            [0.28, 0.52, 0.2],
+            [0.291667, 0.416667, 0.291667],
+            [0.291667, 0.416667, 0.291667],
+            [0.28, 0.52, 0.2],
+            [0.36, 0.44, 0.2],
+            [0.36, 0.44, 0.2],
+            [0.36, 0.44, 0.2],
+            [0.26923, 0.423077, 0.307692],
+            [0.36, 0.44, 0.2],
+            [0.26923, 0.423077, 0.307692],
+        ]
     )
+
+    assert_allclose(out, expected, rtol=1e-5)
+
+    # Test parallel transform
+    encoder.set_params(n_jobs=2)
+    out = encoder.transform(df[["x1"]])
+
+    assert_allclose(out, expected, rtol=1e-5)
